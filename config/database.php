86511de44097dcb730821fe4f74f1794707d9bf6
JIAWENWENN <wenjiakee030403@gmail.com>
2026-06-06 00:42:33 +0800

    Add TiDB Cloud database config with SSL support

--- config/database.php
+++ config/database.php
@@ -60,6 +60,9 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                env('MYSQL_ATTR_SSL_VERIFY_SERVER_CERT') !== null
+                    ? PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => (bool) env('MYSQL_ATTR_SSL_VERIFY_SERVER_CERT')
+                    : null,
             ]) : [],
         ],
 
